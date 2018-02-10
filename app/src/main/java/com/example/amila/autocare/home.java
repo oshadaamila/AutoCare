fd889b83979fcbf2c982a0bd77a3a15763130527
oshadaamila <oshadaamila.15@cse.mrt.ac.lk>
2018-02-10 17:04:40 +0530

    navigation_bar_system

--- app/src/main/java/com/example/amila/autocare/home.java
+++ app/src/main/java/com/example/amila/autocare/home.java
@@ -1,13 +1,37 @@
 package com.example.amila.autocare;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class home extends AppCompatActivity {
-
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        btn = findViewById(R.id.button3);
+        finish();
+        startActivity(new Intent(getApplicationContext(),navigationDrawer.class));
+
+
     }
 }
