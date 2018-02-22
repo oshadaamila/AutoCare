248237b654ceacf596fd478d49e7d8be024dd128
oshadaamila <oshadaamila.15@cse.mrt.ac.lk>
2018-02-22 16:23:59 +0530

    before_mid

---
+++ app/src/main/java/com/example/amila/autocare/Vehicle.java
@@ -0,0 +1,23 @@
+package com.example.amila.autocare;
+
+/**
+ * Created by amila on 2/21/18.
+ */
+
+public class Vehicle {
+
+    public String brand,model,reg_no,insurance_expiry_date,revenue_license_expiry_date;
+
+    public Vehicle(){
+
+    }
+
+    public Vehicle(String brand,String model, String reg_no, String insurance_expiry_date,
+                   String revenue_license_expiry_date){
+        this.brand=brand;
+        this.model = model;
+        this.reg_no=reg_no;
+        this.insurance_expiry_date = insurance_expiry_date;
+        this.revenue_license_expiry_date = revenue_license_expiry_date;
+    }
+}
--- app/src/main/java/com/example/amila/autocare/select_vehicle.java
+++ app/src/main/java/com/example/amila/autocare/select_vehicle.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.icu.util.Calendar;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -15,18 +16,27 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class select_vehicle extends AppCompatActivity {
-    TextView insurance_date;
+    TextView tv_insurance_date,tv_model,tv_revenue_license_expiry,tv_reg_no;
+    Spinner spinner_brand;
     private DatabaseReference mDatabase;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_vehicle);
-        mDatabase = FirebaseDatabase.getInstance().getReference("Vehicle");
+        mDatabase = FirebaseDatabase.getInstance().getReference("Vehicles");
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser mUser = mAuth.getCurrentUser();
         Spinner spinner = (Spinner) findViewById(R.id.spinner_select_brand);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -35,12 +45,40 @@ public class select_vehicle extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-        insurance_date = findViewById(R.id.insurance_date);
+        tv_insurance_date = findViewById(R.id.insurance_date);
+        spinner_brand=findViewById(R.id.spinner_select_brand);
+        tv_model= findViewById(R.id.editText_model);
+        tv_reg_no = findViewById(R.id.editText_reg_no);
+        tv_revenue_license_expiry =findViewById(R.id.revenue_license_expiry);
+
         findViewById(R.id.button_submit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userId = mDatabase.push().getKey();
-                mDatabase.child(userId).setValue("this is a test value");
+                String brand,model,reg_no,insurance_expiry,revenue_license_expiry;
+                brand = spinner_brand.getSelectedItem().toString().trim();
+                model= tv_model.getText().toString().trim();
+                reg_no = tv_reg_no.getText().toString().trim();
+                insurance_expiry = tv_insurance_date.getText().toString().trim();
+                revenue_license_expiry=tv_revenue_license_expiry.getText().toString().trim();
+                Vehicle vehicle = new Vehicle(brand,model,reg_no,insurance_expiry,revenue_license_expiry);
+                String userId = mUser.getUid();
+                mDatabase.child(userId).setValue(vehicle).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            tv_reg_no.setText("");
+                            tv_model.setText("");
+                            tv_insurance_date.setText("");
+                            tv_revenue_license_expiry.setText("");
+                            Toast.makeText(getApplicationContext(),"Vehicle Added",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Check Values",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
             }
         });
 
